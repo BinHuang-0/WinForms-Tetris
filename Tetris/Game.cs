c198b3efaa3e2bafcc150d4b8d390f1a8f5b5701
Restyled.io <commits@restyled.io>
2020-05-02 15:15:38 +0000

    Restyled by astyle

--- Tetris/Game.cs
+++ Tetris/Game.cs
@@ -9,31 +9,31 @@ using System.Windows.Forms;
 
 namespace ProjectTet
 {
-    class Game
-    {
-        //(y,x)
-        private int[,] _Board;
-        private int _Gamespeed;
-        private Piece _curPiece;
+class Game
+{
+    //(y,x)
+    private int[,] _Board;
+    private int _Gamespeed;
+    private Piece _curPiece;
 
-        public int[,] Board {
-            get {
-                return _Board;
-            }
+    public int[,] Board {
+        get {
+            return _Board;
         }
-        public int Gamespeed {
-            get {
-                return _Gamespeed;
-            }
-            set {
-                _Gamespeed = value;
-            }
+    }
+    public int Gamespeed {
+        get {
+            return _Gamespeed;
         }
-        public Game()
-        {
-            _Board = new int[20, 10];
-            _Gamespeed = 1;
-            _curPiece = new IPiece();
+        set {
+            _Gamespeed = value;
+        }
+    }
+    public Game()
+    {
+        _Board = new int[20, 10];
+        _Gamespeed = 1;
+        _curPiece = new IPiece();
 //            for(int i = 0; i < 4; i++)
 //            {
 //                for(int k = 0; k < 4; k++)
@@ -41,33 +41,33 @@ namespace ProjectTet
 //                    //_Board[i + piece.YPosition, k + piece.XPosition] = piece.Shape[i, k] * 2;
 //                }
 //            }
-        }
-
-        public void gameTick()
-        {
-            _curPiece.WritePiece(Board);
-            if (!_curPiece.IsBottom(_Board))
-            {
-                _curPiece.MoveDown();
-            }
-            else
-                this._NewPiece();
-        }
-
-        private void _NewPiece()
-        {
-            _curPiece = new IPiece();
-        }
-
-        public void RemovePiece()
-        {
-            _curPiece.RemovePiece(_Board);
-        }
-
-        public bool gameCheckBottom()
-        {
-            return _curPiece.IsBottom(_Board);
-        }
-
     }
+
+    public void gameTick()
+    {
+        _curPiece.WritePiece(Board);
+        if (!_curPiece.IsBottom(_Board))
+        {
+            _curPiece.MoveDown();
+        }
+        else
+            this._NewPiece();
+    }
+
+    private void _NewPiece()
+    {
+        _curPiece = new IPiece();
+    }
+
+    public void RemovePiece()
+    {
+        _curPiece.RemovePiece(_Board);
+    }
+
+    public bool gameCheckBottom()
+    {
+        return _curPiece.IsBottom(_Board);
+    }
+
+}
 }
--- Tetris/Piece.cs
+++ Tetris/Piece.cs
@@ -8,64 +8,74 @@ using System.Windows.Forms;
 
 namespace ProjectTet
 {
-    public abstract class Piece
+public abstract class Piece
+{
+    public virtual int[,] Shape {
+        get;
+    }
+    public virtual int XPosition {
+        get;
+        set;
+    }
+    public virtual int YPosition {
+        get;
+        set;
+    }
+    public virtual int PieceValue {
+        get;
+    }
+
+    //check bottom
+    public abstract bool IsBottom(int[,] board);
+    //check wall
+    public abstract bool CheckWall(int pos);
+    //rotate
+    public abstract void RotateRight();
+    public abstract void RotateLeft();
+    //move left and right
+    public void MoveLeft()
     {
-        public virtual int[,] Shape { get; }
-        public virtual int XPosition { get; set; }
-        public virtual int YPosition { get; set; }
-        public virtual int PieceValue { get; }
+        XPosition++;
+    }
+    public void MoveRight()
+    {
+        XPosition--;
+    }
 
-        //check bottom
-        public abstract bool IsBottom(int[,] board);
-        //check wall
-        public abstract bool CheckWall(int pos);
-        //rotate
-        public abstract void RotateRight();
-        public abstract void RotateLeft();
-        //move left and right
-        public void MoveLeft()
-        {
-            XPosition++;
-        }
-        public void MoveRight()
-        {
-            XPosition--;
-        }
+    //move down
+    public void MoveDown()
+    {
+        YPosition++;
+    }
 
-        //move down
-        public void MoveDown()
+    //put piece onto board
+    //need to remove previous piece before placing
+    public void WritePiece(int[,] board)
+    {
+        for(int i = 0; i < 4; i++)
         {
-            YPosition++;
-        }
-
-        //put piece onto board
-        //need to remove previous piece before placing
-        public void WritePiece(int[,] board)
-        {
-            for(int i = 0; i < 4; i++)
+            for(int k = 0; k < 4; k++)
             {
-                for(int k = 0; k < 4; k++)
+                if(YPosition + i < 20 && board[YPosition + i, XPosition + k] == 0)
                 {
-                    if(YPosition + i < 20 && board[YPosition + i, XPosition + k] == 0)
-                    {
-                        board[YPosition + i, XPosition + k] = Shape[i, k];
-                    }
+                    board[YPosition + i, XPosition + k] = Shape[i, k];
                 }
-            }           
-        }
-
-        public void RemovePiece(int[,] board)
-        {
-            for(int i = 0; i < 4; i++)
-            {
-                for(int k = 0; k < 4; k++)
-                {
-                    if(YPosition + i < 20 && board[YPosition + i, XPosition + k] == PieceValue)
-                    {
-                        board[YPosition + i, XPosition + k] = 0;
-                    }
-                }
-            }       
+            }
         }
     }
+
+    public void RemovePiece(int[,] board)
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            for(int k = 0; k < 4; k++)
+            {
+                if(YPosition + i < 20 && board[YPosition + i, XPosition + k] == PieceValue)
+                {
+                    board[YPosition + i, XPosition + k] = 0;
+                }
+            }
+        }
+    }
+}
 }
