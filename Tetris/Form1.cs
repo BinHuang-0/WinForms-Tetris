62b19d58ea62f530b7f33caa3aaede66625a716e
Restyled.io <commits@restyled.io>
2020-05-02 15:15:38 +0000

    Restyled by clang-format

--- Tetris/Form1.cs
+++ Tetris/Form1.cs
@@ -9,78 +9,64 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ProjectTet
-{
-public partial class Form1 : Form
-{
-    Game game;
-    Timer gameTimer;
-    Graphics graphics;
-    int size;
+namespace ProjectTet {
+public partial class Form1 : Form {
+  Game game;
+  Timer gameTimer;
+  Graphics graphics;
+  int size;
 
-    public Form1()
-    {
-        InitializeComponent();
-        scoreBox.Text = 0.ToString();
-        levelBox.Text = 1.ToString();
-        linesBox.Text = 0.ToString();
-        game = new Game();
-        graphics = gameBoard.CreateGraphics();
-        size = 20;
+  public Form1() {
+    InitializeComponent();
+    scoreBox.Text = 0.ToString();
+    levelBox.Text = 1.ToString();
+    linesBox.Text = 0.ToString();
+    game = new Game();
+    graphics = gameBoard.CreateGraphics();
+    size = 20;
+  }
+
+  private void DrawBoard() {
+    //            Graphics graphics = gameBoard.CreateGraphics();
+    //            int size = 20;
+
+    for (int i = 0; i < 20; i++) {
+      for (int k = 0; k < 10; k++) {
+        if (game.Board[i, k] != 0)
+          graphics.FillRectangle(new SolidBrush(Color.Red),
+                                 new Rectangle(k * size, i * size, size, size));
+        else
+          graphics.FillRectangle(new SolidBrush(Color.White),
+                                 new Rectangle(k * size, i * size, size, size));
+        graphics.DrawRectangle(new Pen(Color.Gray),
+                               new Rectangle(k * size, i * size, size, size));
+      }
     }
+    if (!game.gameCheckBottom())
+      game.RemovePiece();
+  }
 
-    private void DrawBoard()
-    {
-//            Graphics graphics = gameBoard.CreateGraphics();
-//            int size = 20;
+  private void gameBoard_Click(object sender, EventArgs e) {}
 
-        for(int i = 0; i < 20; i++)
-        {
-            for(int k = 0; k < 10; k++)
-            {
-                if (game.Board[i, k] != 0)
-                    graphics.FillRectangle(new SolidBrush(Color.Red), new Rectangle(k * size, i * size, size, size));
-                else
-                    graphics.FillRectangle(new SolidBrush(Color.White), new Rectangle(k * size, i * size, size, size));
-                graphics.DrawRectangle(new Pen(Color.Gray), new Rectangle(k * size,i * size,size,size));
-            }
-        }
-        if(!game.gameCheckBottom())
-            game.RemovePiece();
-    }
+  private void pictureBox1_Click(object sender, EventArgs e) {}
 
-    private void gameBoard_Click(object sender, EventArgs e)
-    {
-    }
+  private void nextBox_Click(object sender, EventArgs e) {}
 
-    private void pictureBox1_Click(object sender, EventArgs e)
-    {
+  private void playButton_Click(object sender, EventArgs e) {
+    playButton.Hide();
+    InitTimer();
+  }
 
-    }
+  private void InitTimer() {
+    gameTimer = new Timer();
+    gameTimer.Tick += new EventHandler(gameTick);
+    gameTimer.Interval = 1000 / game.Gamespeed;
+    gameTimer.Start();
+  }
 
-    private void nextBox_Click(object sender, EventArgs e)
-    {
-
-    }
-
-    private void playButton_Click(object sender, EventArgs e)
-    {
-        playButton.Hide();
-        InitTimer();
-    }
-
-    private void InitTimer()
-    {
-        gameTimer = new Timer();
-        gameTimer.Tick += new EventHandler(gameTick);
-        gameTimer.Interval = 1000 / game.Gamespeed;
-        gameTimer.Start();
-    }
-
-    private void gameTick(object sender, EventArgs e)
-    {
-        DrawBoard();
-        game.gameTick();
-    }
+  private void gameTick(object sender, EventArgs e) {
+    DrawBoard();
+    game.gameTick();
+  }
 }
 }
--- Tetris/Piece.cs
+++ Tetris/Piece.cs
@@ -6,76 +6,54 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ProjectTet
-{
-public abstract class Piece
-{
-    public virtual int[,] Shape {
-        get;
-    }
-    public virtual int XPosition {
-        get;
-        set;
-    }
-    public virtual int YPosition {
-        get;
-        set;
-    }
-    public virtual int PieceValue {
-        get;
-    }
+namespace ProjectTet {
+public abstract class Piece {
+  public virtual int[, ] Shape { get; }
+  public virtual int XPosition {
+    get;
+    set;
+  }
+  public virtual int YPosition {
+    get;
+    set;
+  }
+  public virtual int PieceValue { get; }
 
-    //check bottom
-    public abstract bool IsBottom(int[,] board);
-    //check wall
-    public abstract bool CheckWall(int pos);
-    //rotate
-    public abstract void RotateRight();
-    public abstract void RotateLeft();
-    //move left and right
-    public void MoveLeft()
-    {
-        XPosition++;
-    }
-    public void MoveRight()
-    {
-        XPosition--;
-    }
+  // check bottom
+  public abstract bool IsBottom(int[, ] board);
+  // check wall
+  public abstract bool CheckWall(int pos);
+  // rotate
+  public abstract void RotateRight();
+  public abstract void RotateLeft();
+  // move left and right
+  public void MoveLeft() { XPosition++; }
+  public void MoveRight() { XPosition--; }
 
-    //move down
-    public void MoveDown()
-    {
-        YPosition++;
-    }
+  // move down
+  public void MoveDown() { YPosition++; }
 
-    //put piece onto board
-    //need to remove previous piece before placing
-    public void WritePiece(int[,] board)
-    {
-        for(int i = 0; i < 4; i++)
-        {
-            for(int k = 0; k < 4; k++)
-            {
-                if(YPosition + i < 20 && board[YPosition + i, XPosition + k] == 0)
-                {
-                    board[YPosition + i, XPosition + k] = Shape[i, k];
-                }
-            }
+  // put piece onto board
+  // need to remove previous piece before placing
+  public void WritePiece(int[, ] board) {
+    for (int i = 0; i < 4; i++) {
+      for (int k = 0; k < 4; k++) {
+        if (YPosition + i < 20 && board[YPosition + i, XPosition + k] == 0) {
+          board[YPosition + i, XPosition + k] = Shape[i, k];
         }
+      }
     }
+  }
 
-    public void RemovePiece(int[,] board)
-    {
-        for(int i = 0; i < 4; i++)
-        {
-            for(int k = 0; k < 4; k++)
-            {
-                if(YPosition + i < 20 && board[YPosition + i, XPosition + k] == PieceValue)
-                {
-                    board[YPosition + i, XPosition + k] = 0;
-                }
-            }
+  public void RemovePiece(int[, ] board) {
+    for (int i = 0; i < 4; i++) {
+      for (int k = 0; k < 4; k++) {
+        if (YPosition + i < 20 &&
+            board[YPosition + i, XPosition + k] == PieceValue) {
+          board[YPosition + i, XPosition + k] = 0;
         }
+      }
     }
+  }
 }
 }
