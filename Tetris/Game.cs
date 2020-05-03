780392f39f38ea3a74c4c81871e056abd34b9465
Restyled.io <commits@restyled.io>
2020-05-03 01:24:04 +0000

    Restyled by astyle

--- Tetris/Game.cs
+++ Tetris/Game.cs
@@ -9,72 +9,78 @@ using System.Windows.Forms;
 
 namespace ProjectTet {
 class Game {
-  //(y,x)
-  private int[, ] _Board;
-  private int _Gamespeed;
-  private Piece _curPiece;
+    //(y,x)
+    private int[, ] _Board;
+    private int _Gamespeed;
+    private Piece _curPiece;
 
-  public int[, ] Board {
-    get { return _Board; }
-  }
-  public int Gamespeed {
-    get { return _Gamespeed; }
-    set { _Gamespeed = value; }
-  }
-  public Game() {
-    _Board = new int[20, 10];
-    _Gamespeed = 100;
-      _NewPiece();
-  }
+    public int[, ] Board {
+        get {
+            return _Board;
+        }
+    }
+    public int Gamespeed {
+        get {
+            return _Gamespeed;
+        }
+        set {
+            _Gamespeed = value;
+        }
+    }
+    public Game() {
+        _Board = new int[20, 10];
+        _Gamespeed = 100;
+        _NewPiece();
+    }
 
-  public void gameTick() {
-    _curPiece.RemovePiece(_Board);
+    public void gameTick() {
+        _curPiece.RemovePiece(_Board);
 //    _curPiece.WritePiece(Board);
-    if (!_curPiece.IsBottom(_Board)) {
-        _curPiece.MoveDown();
-        _curPiece.WritePiece(Board);
-    } else {
-        _curPiece.PieceValue -= 7;
-        _curPiece.WritePiece(Board);
+        if (!_curPiece.IsBottom(_Board)) {
+            _curPiece.MoveDown();
+            _curPiece.WritePiece(Board);
+        } else {
+            _curPiece.PieceValue -= 7;
+            _curPiece.WritePiece(Board);
 //        printBoard();
-        this._NewPiece();
-      }
+            this._NewPiece();
+        }
 
-  }
+    }
 
     private void _NewPiece()
     {
-      _curPiece = new IPiece();
-      _curPiece.PieceValue += 7;
+        _curPiece = new IPiece();
+        _curPiece.PieceValue += 7;
     }
 
 //  public void RemovePiece() { _curPiece.RemovePiece(_Board); }
 //
 //  public bool gameCheckBottom() { return _curPiece.IsBottom(_Board); }
 
-  private void printBoard() {
-    for(int i = 0; i < 20; i++) {
-      for(int k = 0; k < 10; k++) {
-          Debug.Write(Board[i, k]);
-      }
+    private void printBoard() {
+        for(int i = 0; i < 20; i++) {
+            for(int k = 0; k < 10; k++) {
+                Debug.Write(Board[i, k]);
+            }
+            Debug.WriteLine("");
+        }
         Debug.WriteLine("");
     }
-    Debug.WriteLine("");
-  }
-        public void ButtonPress(object sender, KeyEventArgs e) {
-            switch(e.KeyData) {
-                case Keys.Left:
-                    if(_curPiece.CheckWall(0,Board))
-                        _curPiece.MoveLeft();
-                    break;
-                case Keys.Right:
-                    if(_curPiece.CheckWall(1,Board))
-                        _curPiece.MoveRight();
-                    break;
-                case Keys.Down:
-                    _curPiece.MoveDown();
-                    break;
-            }
+    public void ButtonPress(object sender, KeyEventArgs e) {
+        switch(e.KeyData) {
+        case Keys.Left:
+            if(_curPiece.CheckWall(0,Board))
+                _curPiece.MoveLeft();
+            break;
+        case Keys.Right:
+            if(_curPiece.CheckWall(1,Board))
+                _curPiece.MoveRight();
+            break;
+        case Keys.Down:
+            _curPiece.MoveDown();
+            break;
         }
     }
+}
 }
--- Tetris/IPiece.cs
+++ Tetris/IPiece.cs
@@ -7,75 +7,88 @@ using System.Windows.Forms;
 
 namespace ProjectTet {
 class IPiece : Piece {
-  private int[, ] _Shape;
-  private int _XPosition;
-  private int _YPosition;
+    private int[, ] _Shape;
+    private int _XPosition;
+    private int _YPosition;
 
-  public override int[, ] Shape {
-    get { return _Shape; }
-  }
-  public override int XPosition {
-    get { return _XPosition; }
-    set {
-        // use private function CheckWall to check if x is allowed
-      if (value >= 0 && value < 7)
-        _XPosition = value;
-    }
-  }
-
-  public override int YPosition {
-    get { return _YPosition; }
-    set {
-      if (value >= 0 && value < 20)
-        _YPosition = value;
-    }
-  }
-
-  public override int PieceValue { get; set; }
-  public IPiece() {
-    // set inital rotation state
-    _Shape =
-        new int[, ]{{0, 0, 0, 0}, {0, 0, 0, 0}, {1, 1, 1, 1}, {0, 0, 0, 0}};
-    // set position to middle
-    _XPosition = 3;
-    _YPosition = 0;
-      PieceValue = 1;
-  }
-
-  public override bool IsBottom(int[, ] board) {
-    if (_YPosition == 17)
-      return true;
-    for(int i = 0; i < 4; i++)
-    {
-        if (board[_YPosition + 3, _XPosition + i] != 0)
-            return true;
-    }
-    return false;
-  }
-
-        public override bool CheckWall(int direction, int[,] board) {
-            //left
-            if (direction == 0) {
-                //horizontal
-                if(XPosition == 0 || board[YPosition, XPosition - 1] != 0) {
-                    return false;
-                }
-                //vertical
-            }
-            //right
-            else{
-                //horizontal
-                if(XPosition == 6 || board[YPosition, XPosition + 4] != 0) {
-                    return false;
-                }
-                //vertical
-            }
-            return true;
+    public override int[, ] Shape {
+        get {
+            return _Shape;
         }
+    }
+    public override int XPosition {
+        get {
+            return _XPosition;
+        }
+        set {
+            // use private function CheckWall to check if x is allowed
+            if (value >= 0 && value < 7)
+                _XPosition = value;
+        }
+    }
 
-  // see if i can just rotate the array
-  public override void RotateRight() { throw new NotImplementedException(); }
+    public override int YPosition {
+        get {
+            return _YPosition;
+        }
+        set {
+            if (value >= 0 && value < 20)
+                _YPosition = value;
+        }
+    }
 
-  public override void RotateLeft() { throw new NotImplementedException(); }
+    public override int PieceValue {
+        get;
+        set;
+    }
+    public IPiece() {
+        // set inital rotation state
+        _Shape =
+        new int[, ] {{0, 0, 0, 0}, {0, 0, 0, 0}, {1, 1, 1, 1}, {0, 0, 0, 0}};
+        // set position to middle
+        _XPosition = 3;
+        _YPosition = 0;
+        PieceValue = 1;
+    }
+
+    public override bool IsBottom(int[, ] board) {
+        if (_YPosition == 17)
+            return true;
+        for(int i = 0; i < 4; i++)
+        {
+            if (board[_YPosition + 3, _XPosition + i] != 0)
+                return true;
+        }
+        return false;
+    }
+
+    public override bool CheckWall(int direction, int[,] board) {
+        //left
+        if (direction == 0) {
+            //horizontal
+            if(XPosition == 0 || board[YPosition, XPosition - 1] != 0) {
+                return false;
+            }
+            //vertical
+        }
+        //right
+        else {
+            //horizontal
+            if(XPosition == 6 || board[YPosition, XPosition + 4] != 0) {
+                return false;
+            }
+            //vertical
+        }
+        return true;
+    }
+
+    // see if i can just rotate the array
+    public override void RotateRight() {
+        throw new NotImplementedException();
+    }
+
+    public override void RotateLeft() {
+        throw new NotImplementedException();
+    }
 }
 }
